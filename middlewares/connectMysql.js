915465cec99e6fe2e5a560e11f675462685745eb
TheSecondBread <gamezone2904@gmail.com>
2024-08-23 13:26:27 +0530

    port

--- middlewares/connectMysql.js
+++ middlewares/connectMysql.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: 'avnadmin',         
   password: process.env.PASSWORD,  
   database: process.env.DATABASE,
-  port:process.env.PORT
+  port:process.env.SQLPORT
 
 });
 
